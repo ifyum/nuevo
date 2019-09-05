87b66364adbe5d2f93ccb6c97562eae60707ba24
ifyum <ifyumx@gmail.com>
2019-09-05 20:56:02 -0300

    pasar textos desde el back a la vista

--- src/main/java/com/example/tutorial/spring/web/app/controller/IndexController.java
+++ src/main/java/com/example/tutorial/spring/web/app/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.tutorial.spring.web.app.controller;
 
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class IndexController {
     @GetMapping(value="/index")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("titulo","Ifyum");
+        model.addAttribute("cuerpo","hola esto viene desde el back a la vista");
+
+
         return "index";
     }
 }
