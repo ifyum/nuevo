8b711372237877e68d28f933b0dada67257be75f
ifyum <ifyumx@gmail.com>
2019-09-05 21:40:20 -0300

    curso RquestMapping ruta de primer nivel arreglo 4

--- src/main/java/com/example/tutorial/spring/web/app/controller/VideosController.java
+++ src/main/java/com/example/tutorial/spring/web/app/controller/VideosController.java
@@ -1,17 +1,19 @@
 package com.example.tutorial.spring.web.app.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/videos")
 public class VideosController {
 
-    @GetMapping({"/videos", "/" , "" ,  "/Videos"})
+    @GetMapping({"/video", "/" , "" ,  "/Videos"})
     public String videos(Model model){ //ModelMap hace lo mismo que Model
         String texto = "-esto es una variable-";
         model.addAttribute("titulo","Ifyum");
-        model.addAttribute("cuerpo","hola esto viene desde el back a la vista"+texto);
+        model.addAttribute("cuerpo","hola esto viene desde el back a la vista "+texto);
         return "videos";
     }
 
