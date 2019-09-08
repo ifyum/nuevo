8ac9fd30407f3454f56e14259d9e7cc397c4d8e3
ifyum <ifyumx@gmail.com>
2019-09-08 16:18:05 -0300

    tabla relacionada trayendo el dato

--- src/main/java/com/example/tutorial/spring/web/app/controller/ProfesoresController.java
+++ src/main/java/com/example/tutorial/spring/web/app/controller/ProfesoresController.java
@@ -1,15 +1,14 @@
 package com.example.tutorial.spring.web.app.controller;
 
 import com.example.tutorial.spring.web.app.domain.Profesores;
-import com.example.tutorial.spring.web.app.models.Services.ProfesoresService;
 import com.example.tutorial.spring.web.app.models.dto.ProfesoresDTO;
 import com.example.tutorial.spring.web.app.repository.ProfesoresRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import sun.misc.Request;
 
 import java.util.List;
 
@@ -17,12 +16,13 @@ import java.util.List;
 public class ProfesoresController {
     @Autowired
     private ProfesoresRepository profesoresRepository;
-    private ProfesoresService profesoresService;
-    private List<ProfesoresDTO>  profesoresDTO;
+
 
     @RequestMapping( value ="/profesores",method=RequestMethod.GET)
         public String listar(Model model){
-//        profesoresDTO = profesoresRepository.findAll();
+
+
+
         model.addAttribute("titulo","Ifyum");
         model.addAttribute("profesores",profesoresRepository.findAll());
         return "profesores";
