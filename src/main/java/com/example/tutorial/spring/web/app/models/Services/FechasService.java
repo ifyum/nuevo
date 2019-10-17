2c228eb39c71693212c4e183a1d7f353120add71
Ifyum <ifyumx@gmail.com>
2019-10-17 20:40:17 -0300

    contador de dias arreglado

--- src/main/java/com/example/tutorial/spring/web/app/models/Services/FechasService.java
+++ src/main/java/com/example/tutorial/spring/web/app/models/Services/FechasService.java
@@ -60,11 +60,13 @@ public class FechasService {
         int diffDays = 0;
 //
 //        while (fechaInicial.before(fechaFinal) || fechaInicial.equals(fechaFinal)) {
-        while  (dias >= diffDays) {
+        while  (dias != diffDays) {
             System.out.println("dias a sumar: "+dias);
             System.out.println("contador de dias: "+diffDays);
             System.out.println("entro al while ");
+            fechaInicial.add(Calendar.DAY_OF_YEAR,1);
     if (listaFechasNoLaborables != null) {
+
         for (Date date : listaFechasNoLaborables) {
             Date fechaNoLaborablecalendar = fechaInicial.getTime();
             //si el dia de la semana de la fecha minima es diferente de sabado o domingo
@@ -78,24 +80,26 @@ public class FechasService {
             }
         }
     } else {
+
         if (fechaInicial.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY && fechaInicial.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY) {
             //se aumentan los dias de diferencia entre min y max
             diffDays=diffDays+1;
             System.out.println("contador: "+diffDays);
-            fechaInicial.add(Calendar.DAY_OF_YEAR,1);
+
             System.out.println("sumando dias: "+fechaInicial.getTime());
         }
     }
     if (diaHabil == true) {
 
         diffDays=diffDays+1;
-        fechaInicial.add(Calendar.DAY_OF_YEAR,1);
+
     }
-    //se suma 1 dia para hacer la validacion del siguiente dia.
-    fechaInicial.add(Calendar.DATE, 1);
+
+
 //        }
             }
-
+        //se suma 1 dia para hacer la validacion del siguiente dia.
+//        fechaInicial.add(Calendar.DATE, 1);
     fechaFinal.setTime(fechaInicial.getTime());
 
         System.out.println("terminado el cliclo fecha final:"+fechaFinal.getTime());
